4889102524731b576a82668143be1a5052b12bcd
LuSkywalker <callumlu@gmail.com>
2023-04-13 16:43:04 +0800

    [A] at-start-f415 sample code (led)

--- app/inc/at32f415_clock.h
+++ app/inc/at32f415_clock.h
@@ -1,30 +1,7 @@
 /**
   **************************************************************************
-  * @file     main.h
-  * @brief    main header file
-  **************************************************************************
-  *                       Copyright notice & Disclaimer
-  *
-  * The software Board Support Package (BSP) that is made available to
-  * download from Artery official website is the copyrighted work of Artery.
-  * Artery authorizes customers to use, copy, and distribute the BSP
-  * software and its related documentation for the purpose of design and
-  * development in conjunction with Artery microcontrollers. Use of the
-  * software is governed by this copyright notice and the following disclaimer.
-  *
-  * THIS SOFTWARE IS PROVIDED ON "AS IS" BASIS WITHOUT WARRANTIES,
-  * GUARANTEES OR REPRESENTATIONS OF ANY KIND. ARTERY EXPRESSLY DISCLAIMS,
-  * TO THE FULLEST EXTENT PERMITTED BY LAW, ALL EXPRESS, IMPLIED OR
-  * STATUTORY OR OTHER WARRANTIES, GUARANTEES OR REPRESENTATIONS,
-  * INCLUDING BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY,
-  * FITNESS FOR A PARTICULAR PURPOSE, OR NON-INFRINGEMENT.
-  *
-  **************************************************************************
-  */
-/**
-  **************************************************************************
-  * @file     at32f415_can.h
-  * @brief    at32f415 can header file
+  * @file     at32f415_clock.h
+  * @brief    header file of clock program
   **************************************************************************
   *                       Copyright notice & Disclaimer
   *
@@ -46,18 +23,22 @@
   */
 
 /* define to prevent recursive inclusion -------------------------------------*/
-#ifndef __AT32F415_CAN_H
-#define __AT32F415_CAN_H
+#ifndef __AT32F415_CLOCK_H
+#define __AT32F415_CLOCK_H
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-
 /* includes ------------------------------------------------------------------*/
+#include "at32f415.h"
+
+/* exported functions ------------------------------------------------------- */
+void system_clock_config(void);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif
+#endif /* __AT32F415_CLOCK_H */
+
